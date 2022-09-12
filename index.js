599ffe1d481865d064c27115215c6b0810bd9483
Emily Fernschild <emilyfernschild@gmail.com>
2022-09-12 16:58:03 -0400

    catch forEach error

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 fetch('https://api.disneyapi.dev/characters')
 .then(res => res.json())
 .then(characters => characters.forEach(renderCharacters))
+.catch(e => console.error)
 
 function renderCharacters(character){
-    const charBar = document.querySelector('.character-bar');
+    const charBar = document.querySelector('#character-bar');
     const spanName = document.createElement('span');
     spanName.textContent = character.data[11].name;
     charBar.append(spanName);
