403a17a1450197f481bf0432a8b256139dd2b962
Emily Fernschild <emilyfernschild@gmail.com>
2022-09-13 11:11:38 -0400

    added character object

--- index.js
+++ index.js
@@ -10,16 +10,33 @@ pullCharacters('7088')
 pullCharacters('4408')
 pullCharacters('2267')
 
+
 const charName = document.querySelector('#char-name');
+const voteLikes = document.querySelector('#like-count');
+const voteDislikes = document.querySelector('#dislike-count');
+const charCard = document.querySelector('#character-info');
 
 function renderCharacters(character){
     const charBar = document.querySelector('#character-bar');
     const spanName = document.createElement('span');
     spanName.textContent = character.name;
-    //console.log(character)
     charBar.append(spanName);
+    
+    //console.log(characterObj.name)
 
     spanName.addEventListener('click', () => {
         charName.textContent = character.name;
+        const characterObj = {
+            name: character.name,
+            image: character.imageUrl,
+            likes: 0,
+            dislikes: 0,
+            films: character.films,
+            tvShows: character.tvShows,
+            videoGames: character.videoGames
+        }
+        const imgObj = document.createElement('img');
+        imgObj.src = characterObj.image;
+        charCard.append(imgObj);
     })
 }
